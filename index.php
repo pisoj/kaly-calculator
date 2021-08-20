a1ce90173c4eb643c99fa5a704ca80fd41c03721
Josip <87895700+pisoj@users.noreply.github.com>
2021-08-20 09:46:29 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE <html>
+<html class="site">
+  <header>
+  <link rel="stylesheet" href="resource/css/main.css">
+  <?php
+    include 'resource/theme/change.php';
+    if ($_SESSION["theme"] == "dark") {
+      include 'resource/theme/dark.html';
+    }
+    else {
+      include 'resource/theme/light.html';
+    }
+  ?>
+    <title>KALY Calculator</title>
+  </header>
+<head>
+   <center><p id="top">KALY Calculator</p></center>
+</head>
+<main>
+   <?php
+     include 'resource/theme/input.html';
+     if (empty($_GET["a"]) or empty($_GET["b"]) or empty($_GET["z"])) {}
+     else {
+       include 'resource/result.php';
+    }
+     include 'resource/input.html';
+   ?>
+</main>
+<footer>
+  <p id="invisible">1</p>
+  <center><p id="bottom">Power by <a id="byLink" target="_blank" href="https://github.com/pisoj/kaly-calculator/">KALY Calculator</a></p></center>
+    <p id="invisible">1</p>
+</footer>
+</html>
